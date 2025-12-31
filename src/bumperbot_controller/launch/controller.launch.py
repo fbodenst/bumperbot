3efc62bdc131e49466270b67db57ddbd0c9a0c09
Fabian Bodensteiner <fbodensteiner@gmail.com>
2025-12-31 12:31:05 +0100

    added controller

--- src/bumperbot_controller/launch/controller.launch.py
+++ src/bumperbot_controller/launch/controller.launch.py
@@ -1,8 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+
+    wheel_radius_arg = DeclareLaunchArgument(
+        "wheel_radius", 
+        default_value="0.033"
+    )
+
+    wheel_separation_arg = DeclareLaunchArgument(
+        "wheel_separation", 
+        default_value="0.17"
+    )
+
+    use_python = LaunchConfiguration("use_python")
+    wheel_radius = LaunchConfiguration("wheel_radius")
+    wheel_separation = LaunchConfiguration("wheel_separation")
 
     joint_sate_broadcaster_spawner = Node(
         package="controller_manager",
@@ -24,7 +40,17 @@ def generate_launch_description():
         ]
     )
 
+    simple_controller_py = Node(
+        package="bumperbot_controller",
+        executable="simple_controller.py",
+        parameters=[{"wheel_radius": wheel_radius,
+                     "wheel_separation": wheel_separation}]
+    )
+
     return LaunchDescription([
+        wheel_radius_arg, 
+        wheel_separation_arg, 
         joint_sate_broadcaster_spawner,
-        simple_controller
+        simple_controller,
+        simple_controller_py
     ])
